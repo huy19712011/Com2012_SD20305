34cc8f1e6f05958fb7c10a9ee89f11720f302ccc
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-03-12 22:30:20 +0700

    Initial commit

---
+++ COM2012_basics_01_SD20305.sql
@@ -0,0 +1,35 @@
+-- 1. Creating database
+create database com2012_SD20305;
+use com2012_SD20305;
+
+-- 2. Creating table
+create table students(
+	id int primary key,
+	fullName nvarchar(50)
+);
+
+-- drop table
+drop table students;
+
+-- alter table
+alter table students
+add email nvarchar(50) not null;
+
+alter table students
+add phone nvarchar(10) not null;
+
+-- rename table
+exec sp_rename students, sinhViens;
+exec sp_rename sinhViens, students;
+
+-- 3. Insert into
+-- single row
+insert into students(id, fullName, email, phone)
+values(1, 'student 1', 'email 1', 'phone 1');
+
+-- multiple rows
+insert into students(id, fullName, email, phone)
+values
+	(2, 'student 2', 'email 2', 'phone 2'),
+	(3, 'student 3', 'email 3', 'phone 3'),
+	(4, 'student 4', 'email 4', 'phone 4');
