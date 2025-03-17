1e20e4ea5153f5527b1e1d3528d86fe30749fa0a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-03-17 07:51:50 +0700

    Update COM2012_basics_01_SD20305.sql

--- COM2012_basics_01_SD20305.sql
+++ COM2012_basics_01_SD20305.sql
@@ -33,3 +33,33 @@ values
 	(2, 'student 2', 'email 2', 'phone 2'),
 	(3, 'student 3', 'email 3', 'phone 3'),
 	(4, 'student 4', 'email 4', 'phone 4');
+
+---------------Data Manipulation--------------------
+-- 1. SELECT
+SELECT TOP 5
+	first_name AS ten,
+	last_name,
+	email
+FROM sales.customers;
+
+-- 1.1 WHERE
+SELECT
+	*
+FROM sales.customers
+WHERE
+	state = 'CA';
+
+--
+SELECT
+	*
+FROM sales.customers
+WHERE
+	state = 'CA' AND phone IS NOT NULL AND city='San Diego';
+
+-- ORDER BY
+SELECT
+	*
+FROM sales.customers
+WHERE
+	state = 'CA' AND phone IS NOT NULL AND city='San Diego'
+ORDER BY first_name DESC; --ASC (default), DESC
